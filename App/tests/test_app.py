4c90932e69f05c8f087458f7b974c049b84b4ed3
FIRAT YALIN <frat.yalin@gmail.com>
2024-12-28 20:50:26 +0100

    Initial commit: Added Flask To-Do API and Dockerfile

---
+++ App/tests/test_app.py
@@ -0,0 +1,14 @@
+import unittest
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Welcome to the To-Do List API!", response.data)
+
+if __name__ == "__main__":
+    unittest.main()
